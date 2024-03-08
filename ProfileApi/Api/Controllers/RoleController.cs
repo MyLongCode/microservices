7c90bbc947db46c5bceb2d5f22b7b3c69cbd55ef
mylongcode <mylongcode@gmail.com>
2024-03-08 15:47:17 +0500

    add role and user controllers

---
+++ ProfileApi/Api/Controllers/RoleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfileApi.Controllers;
+
+/// <summary>
+/// Работа с ролями
+/// </summary>
+[Route("public/role")]
+public class RoleController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
