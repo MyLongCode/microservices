6903e7bd6f0ed1bed5e5bcb1652b841d65427990
mylongcode <mylongcode@gmail.com>
2024-03-28 23:39:44 +0500

    add ef

---
+++ ProfileApi/DAL/EF/UserRoleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProfileDal.Roles.Models;
+using ProfileDal.Users.Models;
+namespace ProfileDal.EF;
+{
+    public class UserRoleDbContext : IdentityDbContext
+    {
+
+        public DbSet<RoleDal> Roles{ get; set; }
+        public DbSet<UserDal> AppUsers{ get; set; }
+
+        public UserRoleDbContext(DbContextOptions<UserRoleDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
